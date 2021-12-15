30ff5ca470bdb9deff143c963b76fc090a911352
Opionz <ope.ajayi@hotmail.com>
2021-12-15 23:07:33 +0100

    Task components implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import { useState } from 'react'  
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+const App = () => {
 
-function App() {
+  const[tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Morning Meditation',
+        day: 'December 14th at 8:30am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Gym',
+        day: 'December 14th at 9:30am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Daily Standup meeting',
+        day: 'December 14th at 11:30am',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: 'Check Fantasy Football Team',
+        day: 'December 14th at 4:30pm',
+        reminder: true,
+    },
+])
+// Delete Task
+ const deleteTask = (id) => {
+  // console.log('delete', id)
+  setTasks(tasks.filter((task) => task.id !== id))
+ }
+
   return (
     <div className="container ">
       <Header  />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({title}) => {
     const onClick = () => {
         console.log('click')
     }
-    return (
+    return ( 
         <header className='header'>
             <h1>{title}</h1>
             <Button color='green' text ='Add' onClick={onClick} />
